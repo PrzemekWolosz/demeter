1eb2c9995a05cbd38e674a2162d641276e08fe92
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-24 09:24:20 +0200

    Initial commit

---
+++ src/test/java/com/przemyslawwolosz/demeter/DemeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przemyslawwolosz.demeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
