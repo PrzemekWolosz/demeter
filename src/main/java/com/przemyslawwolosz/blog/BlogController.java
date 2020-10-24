644e75d51d376a971120e38f8fc8078c0e8c2e7f
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-24 13:39:41 +0200

    Add post.json

--- src/main/java/com/przemyslawwolosz/blog/BlogController.java
+++ src/main/java/com/przemyslawwolosz/blog/BlogController.java
@@ -2,6 +2,8 @@ package com.przemyslawwolosz.blog;
 
 import com.przemyslawwolosz.blog.menu.MenuDto;
 import com.przemyslawwolosz.blog.menu.MenuService;
+import com.przemyslawwolosz.blog.post.PostDto;
+import com.przemyslawwolosz.blog.post.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +15,20 @@ import java.util.List;
 public class BlogController {
 
     @Autowired
+    private PostService postService;
+
+    @Autowired
     private MenuService menuService;
 
     @GetMapping({"/blog"})
     public String getBlog(Model model) {
+
+        List<PostDto> posts = postService.getPosts();
         List<MenuDto> menuItems = menuService.getMenu();
+
+        model.addAttribute("posts", posts);
         model.addAttribute("menuItems", menuItems);
+
         return "blog/index.html";
     }
 }
