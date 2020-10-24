c76bc3e7258621f87c5b3d39f1ad36aeda54dab4
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-24 09:52:28 +0200

    Add HomeController & FragmentsController

---
+++ src/main/java/com/przemyslawwolosz/fragments/FragmentsController.java
@@ -0,0 +1,13 @@
+package com.przemyslawwolosz.fragments;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FragmentsController {
+
+    @GetMapping("/fragments")
+    public String getFragmentsExample() {
+        return "fragments/index.html";
+    }
+}
