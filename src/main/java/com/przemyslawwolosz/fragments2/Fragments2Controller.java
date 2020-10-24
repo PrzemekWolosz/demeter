cab63f5640a7bbc939ced11945f97d498f49d1cd
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-24 10:30:24 +0200

    Add Fragments2Controller

---
+++ src/main/java/com/przemyslawwolosz/fragments2/Fragments2Controller.java
@@ -0,0 +1,13 @@
+package com.przemyslawwolosz.fragments2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Fragments2Controller {
+
+    @GetMapping("/fragments2")
+    public String getFragmentsExample() {
+        return "fragments2/index.html";
+    }
+}
